e22a12def33a09f78b095053cc006e44aabc1749
Thomas Bjerring <Bjerring@users.noreply.github.com>
2020-01-22 20:15:21 +0100

    ini commit

---
+++ app.py
@@ -0,0 +1,266 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 08:46:19 2020
+
+@author: ttb
+"""
+
+#%%
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table as dt
+import base64
+from datetime import datetime, timedelta
+import os
+import pathlib
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import json
+
+#%%
+# get relative data folder
+PATH = pathlib.Path(__file__)#.parent
+#PATH = os.chdir("C:\\Users\\ttb\\Desktop\\projects\\python\\app\\data")
+DATA_PATH = PATH.joinpath("../data").resolve()
+
+#df_current_prices = pd.read_excel("User Interface Exercise.xlsx",sheet_name="Data")
+df_current_prices = pd.read_excel(DATA_PATH.joinpath("User Interface Exercise.xlsx"),sheet_name="Data")
+df_current_prices = df_current_prices.set_index("Dates")
+
+index_name = ["S&P 500", "Euro STOXX","TOPIX"]
+
+returns = df_current_prices[df_current_prices.columns[:3]]
+returns.columns = index_name
+
+bar_notes = df_current_prices[df_current_prices.columns[3:]]
+bar_notes.columns = index_name
+
+notes = df_current_prices[df_current_prices.columns[3:]]
+notes.columns = index_name
+notes.loc[notes.index,"year"] = notes.index.year.tolist()
+notes.loc[notes.index,"month"] = notes.index.month.tolist()
+notes = notes.reset_index()
+notes = notes.drop('Dates',axis=1)
+notes = notes.set_index(['year','month'])
+
+
+################################################
+# FUNCTIONS START
+
+def make_dash_table( df ):
+    ''' Return a dash definition of an HTML table for a Pandas dataframe '''
+    table = []
+    for index, row in df.iterrows():
+        html_row = []
+        for i in range(len(row)):
+            html_row.append( html.Td([ row[i] ]) )
+        table.append( html.Tr( html_row ) )
+    return table
+
+
+################################################
+# APP START
+    
+app = dash.Dash('ADIC')
+
+
+app.layout = html.Div([
+    html.Div([
+        html.Div([
+                #html.H5(" "),
+                html.Img(src=app.get_asset_url("ADICcl.jpg"),width=250),
+                ],className="two columns"),
+
+        html.Div([
+                html.H5("Select Index"),
+                
+                ],className="two columns"),
+
+        html.Div([
+                dcc.Dropdown(
+                        id='dropdown_menu',
+                        options=[{'label':index_name[i], 'value':index_name[i] } for i in range(len(index_name))],    
+                        value=index_name[0])                        
+                ],className="two columns"),
+
+        html.Div([
+                html.H5("Search Comentary"),
+            
+            ],className="two columns"),
+            
+        html.Div([
+                dcc.Input(id="search_input", type="text", placeholder="search"),
+                html.H6("Please use the following format: Index Mmm-YY e.g. S&P 500 Jan-14")
+            
+                ],className="two columns"),
+
+        html.Div([
+                html.H5(" "),
+                
+                ],className="two columns"),
+
+        ],className="row"),
+
+
+    html.Div([
+            html.Div([
+                    dcc.Graph(id="return-tbl", config={'displayModeBar': False}),
+                    html.Table(id='table_perf',style={"margin-bottom": "0px","margin-top": "0px"})
+                    ],className="six columns"),
+
+            html.Div([
+                    html.H4('Results'),
+                    html.Div(id="output"),
+                    ],className="six columns"),
+            
+            ],className="row")
+])
+
+
+    
+### create bar plot displaying the 1, 3, 5 year return
+@app.callback(Output('return-tbl', 'figure'),[Input('dropdown_menu', 'value')])
+def update_return_tbl(value):
+    
+    
+    returns_idx = returns[value]
+    
+    #returns_idx.loc[returns_idx.index >= str(returns_idx.index.year[-1])+'-01-01' ]
+    
+    figure={
+        "data": [
+            go.Bar(
+                x=returns_idx.index,
+                y= returns_idx.round(2),
+                marker={
+                    "color": "#6161FF",
+                    "line": {
+                        "color": "rgb(255, 255, 255)",
+                        "width": 2,
+                    },
+                },
+                name=value,
+                hovertext=bar_notes[value].tolist(),
+            ),
+
+        ],
+        "layout": go.Layout(
+            autosize=False,
+            bargap=0,#.35
+            font={"family": "Raleway", "size": 10},
+            #height=200,
+            hovermode="closest",
+            legend={
+                "x": -0.0228945952895,
+                "y": -0.189563896463,
+                "orientation": "h",
+                "yanchor": "top",
+            },
+            margin={
+                "r": 0,
+                "t": 20,
+                "b": 10,
+                "l": 20,
+            },
+            showlegend=True,
+            title="",
+            #width=330,
+            xaxis={
+                "autorange": True,
+                #"range": [-0.5, 4.5],
+                "showline": True,
+                "title": "",
+                "type": "date",
+            },
+            yaxis={
+                "autorange": True,
+                #"range": [returns_idx.min*0.9, returns_idx.max*1.1],
+                "showgrid": True,
+                "showline": True,
+                "title": "",
+                "type": "linear",
+                "zeroline": False,
+            },
+        ),
+    }
+
+    return figure
+
+
+@app.callback(Output('table_perf', 'children'), [Input('dropdown_menu', 'value')])
+def update_table2(value):
+
+
+    returns_idx = returns[value]
+
+    m1 = returns_idx[-1]
+    m3 = ((1+returns_idx[-3:]/100).cumprod()[-1]-1)*100
+    m6 = ((1+returns_idx[-6:]/100).cumprod()[-1]-1)*100
+    y1 = ((1+returns_idx[-12:]/100).cumprod()[-1]-1)*100
+    y3 = ((1+returns_idx[-3*12:]/100).cumprod()[-1]-1)*100
+    y5 = ((1+returns_idx[-5*12:]/100).cumprod()[-1]-1)*100
+    
+    return_periods = pd.Series({"1 Month":m1,
+                               "3 Month":m3,
+                               "6 Month":m6,
+                               "1 Year":y1,
+                               "3 Year":y3,
+                               "5 Year":y5
+                            }).round(2)
+    
+    out_df = return_periods.to_frame(value).T
+
+    out_df = out_df.reset_index()
+    out_df.columns = ["Index"] + out_df.columns[1:].tolist()
+    
+    data = out_df.to_dict('rows')
+    columns =  [{"name": i, "id": i,} for i in (out_df.columns)]
+    
+    # create datatable 
+    table = dt.DataTable(data=data, columns=columns, style_cell={'fontSize':14, 'font-family':'sans-serif'} ) # , fixed_rows={ 'headers': True, 'data': 0 },style_cell = {'minWidth': '90px', 'maxWidth': '150px'},style_table={'maxHeight': '300px','maxWidth': '680px'}  
+
+
+    return table
+    
+    
+
+@app.callback(
+    Output("output", "children"),
+    [Input("search_input", "value")],
+)
+def update_output(input1):
+    
+    month_conv = pd.Series([1,2,3,4,5,6,7,8,9,10,11,12],index=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
+    
+    
+    if input1 is None:
+        search_out = " "
+        
+    if input1 is not None:    
+        # date
+        date = input1[-6:]
+    
+        # index name
+        idx = input1[:-7]
+        
+        # try and search for notes
+        try:
+            search_out = notes.loc[(float('20'+date[4:]),month_conv[date[:3]] ) , idx]    
+        except:
+            search_out = " "
+
+    return search_out
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+    # Open browser and go to
+    # http://127.0.0.1:8050/
+    # or
+    # http://127.0.0.1:5000/
